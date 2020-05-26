9de14938a809c501905f4a9513cd857d44e1e613
Carlos Castillo <closcastillo95@gmail.com>
2020-05-25 20:28:34 -0700

    fixing links

--- src/Projects.js
+++ src/Projects.js
@@ -23,7 +23,7 @@ class Projects extends React.Component {
 			<div className="browser_projects">
 				<h2>Browser Related Projects</h2>
 				<ul className="browser_projects_menu">
-					<li><a onClick={this.linkHandler.bind(this, "https://github.com/AGuyNamedC-Los/personal_website")}><p>This Website</p></a></li>
+					<li><a onClick={this.linkHandler.bind(this, "https://github.com/AGuyNamedC-Los/personal-website")}><p>This Website</p></a></li>
 					<li><a onClick={this.webPageHandler.bind(this, "SBMockify")}><p>Mockify</p></a></li>
 					<li><a onClick={this.linkHandler.bind(this, "https://github.com/AGuyNamedC-Los/gift-ee")}><p>Giftee</p></a></li>
 				</ul>
