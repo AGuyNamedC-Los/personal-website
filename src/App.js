3235c5abc1f40eeed0e7b267545a6228df1ac8b9
Carlos Castillo <closcastillo95@gmail.com>
2020-05-25 20:10:43 -0700

    setting things up for heroku

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainApp from "./MainApp.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+	constructor(props) {
+        super(props); // Must call
+		this.state = {show: "MainApp"};
+    }
+
+    // Renders component based on current state and props
+    render() {
+		switch (this.state.show) {
+            case 'MainApp':
+                return <MainApp />;
+                break;
+            default:
+                return <h2>Some type of problem!</h2>;
+				break;
+        }
+	}
 }
 
 export default App;
