c6cc756e6a11a7db94338fab65f613da133c2886
Carlos Castillo <closcastillo95@gmail.com>
2020-08-06 18:13:43 -0700

    changed tab logo

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import Home from "./Home";
 import Projects from "./Projects";
 import SBMockify from "./SBMockify";
